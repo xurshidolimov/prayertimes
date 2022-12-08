1dcf542668b91ed7230fc11da8bf1f18990ba7c1
SX Computers <xurshidolimov017@gmail.comgit config --global user.name>
2022-12-08 09:06:56 +0500

    error

--- data/config.py
+++ data/config.py
@@ -10,10 +10,10 @@ BOT_TOKEN = "5473423088:AAH_f6Z1Tg-OtN6Y7BgoVm4BJUq3p3DNXfU"  # Bot toekn
 ADMINS = 679932311  # adminlar ro'yxati
 IP = 'localhost'  # Xosting ip manzili
 
-DB_USER = env.str("DB_USER")
-DB_PASS = env.str("DB_PASS")
-DB_NAME = env.str("DB_NAME")
-DB_HOST = env.str("DB_HOST")
+DB_USER = 'postgres'
+DB_PASS = 'xurshid_olimov'
+DB_NAME = 'prayertimes'
+DB_HOST = 'localhost'
 
 month_now = datetime.datetime.now().month
 day_now = datetime.datetime.now().day
