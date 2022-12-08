635bfa4c93e48c67efde0b633cde48226c8c6311
SX Computers <xurshidolimov017@gmail.comgit config --global user.name>
2022-12-08 10:37:15 +0500

    first commit

--- data/config.py
+++ data/config.py
@@ -6,14 +6,14 @@ env = Env()
 env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = "5473423088:AAH_f6Z1Tg-OtN6Y7BgoVm4BJUq3p3DNXfU"  # Bot toekn
-ADMINS = '679932311'  # adminlar ro'yxati
-IP = 'localhost'  # Xosting ip manzili
+BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+IP = env.str("ip")  # Xosting ip manzili
 
-DB_USER = 'prayertimes'
-DB_PASS = 'turkiston'
-DB_NAME = 'prayertimes'
-DB_HOST = 'localhost'
+DB_USER = env.str("DB_USER")
+DB_PASS = env.str("DB_PASS")
+DB_NAME = env.str("DB_NAME")
+DB_HOST = env.str("DB_HOST")
 
 month_now = datetime.datetime.now().month
 day_now = datetime.datetime.now().day
